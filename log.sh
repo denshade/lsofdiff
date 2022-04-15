ba86bfab6506ab3bff784650875a2cb0565a8f2d
Lieven Vaneeckhaute <lieven.vaneeckhaute@gmail.com>
2022-04-15 19:43:13 +0200

    logging and sleep parameter

--- log.sh
+++ log.sh
@@ -1,12 +1,12 @@
-if [ "$#" -ne 1 ]; then
-       echo "Usage $0 <pid>"
+if [ "$#" -ne 2 ]; then
+       echo "Usage $0 <pid> <sleeptime>"
        exit 1
 fi
 i=0
 while true; do
 
 	lsof -p $1 | rev| cut -d' ' -f1 | rev > logs/$i.log 2> /dev/null      #`date +%y-%m-%d-%H:%M:%S:%N` 2> /dev/null
-	sleep 0.5
+	sleep $2
 	echo "."
 	let "i=i+1"
 done
