9a5bc6cadc6edc38bdb6ee4fa27a1fdfe78486d3
Lieven Vaneeckhaute <lieven.vaneeckhaute@gmail.com>
2022-04-16 11:18:55 +0200

    log, clean dir, same line .

--- log.sh
+++ log.sh
@@ -2,10 +2,13 @@ if [ "$#" -ne 2 ]; then
        echo "Usage $0 <pid> <sleeptime>"
        exit 1
 fi
+echo "recording: control C to stop."
+mkdir -p logs
+rm logs/*.log
 i=0
 while true; do
 	lsof -p $1 -Fn > logs/$i.log 2> /dev/null      #`date +%y-%m-%d-%H:%M:%S:%N` 2> /dev/null
 	sleep $2
-	echo "."
+	echo -n "."
 	let "i=i+1"
 done
